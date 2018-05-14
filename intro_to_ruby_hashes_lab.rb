272090b72e6ad87e585c60e6404a91953ad396f8
Precious Humphrey <phumphrey23@gmail.com>
2018-05-14 16:34:03 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def monopoly_with_second_tier
 	monopoly = {
    :railroads => {
          pieces: 4,
-	       :rent_in_dollars => {}
+	       :rent_in_dollars => {},
 	       :names=> {}
   }
 }
@@ -32,7 +32,7 @@ def monopoly_with_third_tier
 					 two_pieces_owned: 50,
 					 three_pieces_owned: 100,
 					 four_pieces_owned: 200,
-				 }
+				 },
 				 :names=> {
 					 :reading_railroad => {},
 				   :pennsylvania_railroad => {},
@@ -53,7 +53,7 @@ def monopoly_with_fourth_tier
 					 two_pieces_owned: 50,
 					 three_pieces_owned: 100,
 					 four_pieces_owned: 200,
-				 }
+				 },
 				 :names=> {
 					 :reading_railroad => {"mortgage_value" => "$100"},
 					 :pennsylvania_railroad => {"mortgage_value" => "$200"},
